875a71f38283b9179841dd599c1a72f242a11928
Efreitor2001 <pokimoha.123@gmail.com>
2022-04-16 03:17:37 +0300

    Done second task

--- C#_Seminars/HomeWork_2/Program.cs
+++ C#_Seminars/HomeWork_2/Program.cs
@@ -2,39 +2,82 @@
 и на выходе показывает вторую цифру этого числа. */
 /*
 Console.ForegroundColor = ConsoleColor.Green;
-void ShowSecondNum(int num)
-{
-   int num1 = num / 10;
-   int num2 = num1 % 10;
-   Console.ForegroundColor = ConsoleColor.Blue;
-   Console.WriteLine("The second digit in " + num + " is " + num2 + ".");
-}
 Console.Write("Input number: ");
 var number = Console.ReadLine();
-int secondNum = 0;
+int secondNum;
 bool isNum()
 {
     bool IsNum = int.TryParse(number, out secondNum);
     while (IsNum == false)
     {
         Console.ForegroundColor = ConsoleColor.Red;
-        Console.Write("Error, input correct day: ");
+        Console.Write("Error, input correct number: ");
         Console.ForegroundColor = ConsoleColor.Green;
         number = Console.ReadLine();
         IsNum = int.TryParse(number, out secondNum);
     }
     return true;
 }
-while (secondNum < 100 || secondNum > 999)
+isNum();
+void ShowSecondNum(int num)
 {
-   Console.ForegroundColor = ConsoleColor.Red;
-   Console.Write("Error, input correct number: ");
-   Console.ForegroundColor = ConsoleColor.Green;
-   number = Console.ReadLine();
-   isNum();
+    num /= 10;
+    num %= 10;
+    if (num < 0) num *= (-1);
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.WriteLine("The second digit in " + number + " is " + num + ".");
+}
+while ((Convert.ToInt32(number) < 100 || Convert.ToInt32(number) > 999) && (Convert.ToInt32(number) > (-100) || Convert.ToInt32(number) < (-999)))
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.Write("Error, input correct number: ");
+    Console.ForegroundColor = ConsoleColor.Green;
+    number = Console.ReadLine();
+    isNum();
 }
 ShowSecondNum(secondNum);
 */
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.ForegroundColor = ConsoleColor.Green;
+Console.Write("Input number: ");
+var enter = Console.ReadLine();
+int num;
+bool isNum()
+{
+    bool IsNum = int.TryParse(enter, out num);
+    while (IsNum == false)
+    {
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.Write("Error, input correct number: ");
+        Console.ForegroundColor = ConsoleColor.Green;
+        enter = Console.ReadLine();
+        IsNum = int.TryParse(enter, out num);
+    }
+    return true;
+}
+isNum();
+void ThirdNum(int digit)
+{
+    while (digit > 999 || digit < (-999))
+    {
+        digit /= 10;
+    }
+    digit %= 100;
+    digit %= 10;
+    if (digit < 0) digit *= (-1);
+    Console.ForegroundColor = ConsoleColor.Blue;
+    Console.Write("The third digit in " + enter + " is " + digit + ".");
+}
+if (num < 100 && num > (-100))
+{
+    Console.WriteLine("Your number don't have third digit!");
+}
+else
+{
+    ThirdNum(num);
+}
+
 /* Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели,
 и проверяет, является ли этот день выходным. */
 /*
