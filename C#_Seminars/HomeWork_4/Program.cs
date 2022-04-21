dee06adcf835055c50c93e9c805f297a22b1ce1d
Efreitor2001 <pokimoha.123@gmail.com>
2022-04-22 01:39:52 +0300

    Added edited pre-release HW_3

--- C#_Seminars/HomeWork_4/Program.cs
+++ C#_Seminars/HomeWork_4/Program.cs
@@ -1,12 +1,14 @@
-﻿/* Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
+﻿int c;
+/* Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами.
 Напишите программу, которая покажет количество чётных чисел в массиве. */
-int CountArray()
+
+int CountArray(int lengthArray)
 {
-    int[] a = new int[10];
+    int[] a = new int[lengthArray];
     int count = 0;
     for (int i = 0; i < a.Length; i++)
     {
-        a[i] = new Random().Next(100, 1000);
+        a[i] = new Random().Next();
         Console.Write(a[i] + " ");
         if (a[i] % 2 == 0)
         {
@@ -17,19 +19,30 @@ int CountArray()
     return count;
 }
 Console.WriteLine("Task #1");
-Console.WriteLine("Count even numbers in array = " + CountArray());
+Console.Write("Input array lenght: ");
+var length1 = Console.ReadLine();
+bool IsNum1 = int.TryParse(length1, out c);
+while (IsNum1 == false)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.Write("Error, input correct numner: ");
+    Console.ForegroundColor = ConsoleColor.Green;
+    length1 = Console.ReadLine();
+    IsNum1 = int.TryParse(length1, out c);
+}
+Console.WriteLine("Count even numbers in array = " + CountArray(Convert.ToInt32(length1)));
 Console.WriteLine();
 
 /* Задача 36: Задайте одномерный массив, заполненный случайными числами.
 Найдите сумму элементов, стоящих на нечётных позициях. */
 
-int SumArray()
+int SumArray(int lengthArray)
 {
-    int[] b = new int[10];
+    int[] b = new int[lengthArray];
     int sum = 0;
     for (int index = 0; index < b.Length; index++)
     {
-        b[index] = new Random().Next(1, 11);
+        b[index] = new Random().Next();
         Console.Write(b[index] + " ");
         if (index % 2 != 0)
         {
@@ -40,21 +53,32 @@ int SumArray()
     return sum;
 }
 Console.WriteLine("Task #2");
-Console.WriteLine("Sum elements in odd positions = " + SumArray());
+Console.Write("Input array lenght: ");
+var length2 = Console.ReadLine();
+bool IsNum2 = int.TryParse(length2, out c);
+while (IsNum2 == false)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.Write("Error, input correct numner: ");
+    Console.ForegroundColor = ConsoleColor.Green;
+    length2 = Console.ReadLine();
+    IsNum2 = int.TryParse(length2, out c);
+}
+Console.WriteLine("Sum elements in odd positions = " + SumArray(Convert.ToInt32(length2)));
 Console.WriteLine();
 
 /* Задача 38: Задайте массив вещественных чисел.
 Найдите разницу между максимальным и минимальным элементов массива. */
 
-void DiffArray()
+void DiffArray(int lengthArray)
 {
-    double[] c = new double[10];
+    double[] c = new double[lengthArray];
     double max = 0;
     double min = 1;
     for (int ind = 0; ind < c.Length; ind++)
     {
-        c[ind] = new Random().Next(1, 100);
-        Console.Write(c[ind] + " ");
+        c[ind] = new Random().NextDouble();
+        Console.Write(Math.Round(c[ind], 2) + " ");
         if (c[ind] > max)
         {
             max = c[ind];
@@ -69,10 +93,21 @@ void DiffArray()
         }
     }
     Console.WriteLine();
-    Console.WriteLine("Max = " + max);
-    Console.WriteLine("Min = " + min);
-    Console.WriteLine("Difference between Max and Min = " + (max - min));
+    Console.WriteLine("Max = " + Math.Round(max, 2));
+    Console.WriteLine("Min = " + Math.Round(min, 2));
+    Console.WriteLine("Difference between Max and Min = " + Math.Round((max - min), 2));
 }
 Console.WriteLine("Task #3");
-DiffArray();
+Console.Write("Input array lenght: ");
+var length3 = Console.ReadLine();
+bool IsNum3 = int.TryParse(length3, out c);
+while (IsNum3 == false)
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.Write("Error, input correct numner: ");
+    Console.ForegroundColor = ConsoleColor.Green;
+    length3 = Console.ReadLine();
+    IsNum3 = int.TryParse(length3, out c);
+}
+DiffArray(Convert.ToInt32(length3));
 Console.WriteLine();
