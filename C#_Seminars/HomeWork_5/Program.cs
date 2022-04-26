f3a7da405903b78b6a28a45b28601bd9ae991717
Efreitor2001 <pokimoha.123@gmail.com>
2022-04-26 18:05:28 +0300

    Done all HW_5

---
+++ C#_Seminars/HomeWork_5/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 1: Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+Console.Write("Input first number: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second number: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+int result = 1;
+while (count != b)
+{
+    result *= a;
+    count++;
+}
+Console.WriteLine(result);
+
+// Задача 2: Напишите программу, которая принимает на вход число
+// и выдаёт сумму цифр в числе.
+Console.Write("Input Your number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int res = 0;
+while (num / 10 != 0)
+{
+    res += num % 10;
+    num /= 10;
+}
+res += num;
+Console.WriteLine(res); // Самое универсальное решение этой задачи.
+
+// Задача 3: Напишите программу, которая задаёт массив
+// из 8 элементов и выводит их на экран.
+int[] array = new int[8];
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1, 101);
+}
+for (int j = 0; j < array.Length - 1; j++)
+{
+    Console.Write(array[j] + ", ");
+}
+Console.WriteLine(array[7] + "]");
