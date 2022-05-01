00defd921f5ffd3c5734c4480f141b9a35effa4f
Efreitor2001 <pokimoha.123@gmail.com>
2022-05-01 16:58:12 +0300

    Added some fixed for HW_5

--- C#_Seminars/HomeWork_5/Program.cs
+++ C#_Seminars/HomeWork_5/Program.cs
@@ -1,30 +1,38 @@
 ﻿// Задача 1: Напишите цикл, который принимает на вход два числа (A и B)
 // и возводит число A в натуральную степень B.
+double Degree(int num, int num1)
+{
+    int count = 0;
+    int result = 1;
+    while (count != num1)
+    {
+        result *= num;
+        count++;
+    }
+    return result;
+}
 Console.Write("Input first number: ");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.Write("Input second number: ");
 int b = Convert.ToInt32(Console.ReadLine());
-int count = 0;
-int result = 1;
-while (count != b)
-{
-    result *= a;
-    count++;
-}
-Console.WriteLine(result);
+Console.WriteLine(Degree(a, b));
 
 // Задача 2: Напишите программу, которая принимает на вход число
 // и выдаёт сумму цифр в числе.
+double SumDigit(int n)
+{
+    int res = 0;
+    while (n / 10 != 0)
+    {
+        res += n % 10;
+        n /= 10;
+    }
+    res += n;
+    return res;
+}
 Console.Write("Input Your number: ");
 int num = Convert.ToInt32(Console.ReadLine());
-int res = 0;
-while (num / 10 != 0)
-{
-    res += num % 10;
-    num /= 10;
-}
-res += num;
-Console.WriteLine(res); // Самое универсальное решение этой задачи.
+Console.WriteLine(SumDigit(num)); // Самое универсальное решение этой задачи.
 
 // Задача 3: Напишите программу, которая задаёт массив
 // из 8 элементов и выводит их на экран.
