65c5e77939de59e19b47f7f187d8acd80c10b78c
Efreitor2001 <pokimoha.123@gmail.com>
2022-05-16 18:33:03 +0300

    Done all HW_7

--- C#_Seminars/HomeWork_7/Program.cs
+++ C#_Seminars/HomeWork_7/Program.cs
@@ -1,66 +1,66 @@
 ﻿// Задача 47. Задайте двумерный массив размером m×n,
 // заполненный случайными вещественными числами.
-// double[,] CreateArray(int size1, int size2)
-// {
-//     double[,] randomArray = new double[size1, size2];
-//     for (int i = 0; i < randomArray.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < randomArray.GetLength(1); j++)
-//         {
-//             randomArray[i, j] = new Random().NextDouble();
-//         }
-//     }
-//     return randomArray;
-// }
-// void PrintArray(double[,] arr)
-// {
-//     for (int i = 0; i < arr.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < arr.GetLength(1); j++)
-//         {
-//             Console.Write(Math.Round(arr[i, j], 2) + " ");
-//         }
-//         Console.WriteLine();
-//     }
-// }
-// Console.Write($"Iput m number: ");
-// int m = Convert.ToInt32(Console.ReadLine());
-// Console.Write($"Iput n number: ");
-// int n = Convert.ToInt32(Console.ReadLine());
-// PrintArray(CreateArray(m, n));
+double[,] CreateArray(int size1, int size2)
+{
+    double[,] randomArray = new double[size1, size2];
+    for (int i = 0; i < randomArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < randomArray.GetLength(1); j++)
+        {
+            randomArray[i, j] = new Random().NextDouble();
+        }
+    }
+    return randomArray;
+}
+void PrintArray(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write(Math.Round(arr[i, j], 2) + " ");
+        }
+        Console.WriteLine();
+    }
+}
+Console.Write($"Iput m number: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Iput n number: ");
+int n = Convert.ToInt32(Console.ReadLine());
+PrintArray(CreateArray(m, n));
 
 // Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
 // и возвращает значение этого элемента или же указание, что такого элемента нет.
-// int[,] RandomArray()
-// {
-//     int[,] rndmArray = new int[new Random().Next(5, 11), new Random().Next(5, 11)];
-//     for (int i = 0; i < rndmArray.GetLength(0); i++)
-//     {
-//         for (int j = 0; j < rndmArray.GetLength(1); j++)
-//         {
-//             rndmArray[i, j] = new Random().Next(1, 100);
-//         }
-//     }
-//     return rndmArray;
-// }
-// void ShowElement(int[,] array, int pos1, int pos2)
-// {
-//     int a = array.GetLength(0);
-//     int b = array.GetLength(1);
-//     if (pos1 <= a && pos2 <= b && pos1 > 0 && pos2 > 0)
-//     {
-//         Console.WriteLine($"Element in position {pos1} {pos2} = {array[pos1 - 1, pos2 - 1]}");
-//     }
-//     else
-//     {
-//         Console.WriteLine($"Not found element in position {pos1} {pos2}");
-//     }
-// }
-// Console.Write("Input first position: ");
-// int x = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Input second position: ");
-// int y = Convert.ToInt32(Console.ReadLine());
-// ShowElement(RandomArray(), x, y);
+int[,] RandomArray()
+{
+    int[,] rndmArray = new int[new Random().Next(5, 11), new Random().Next(5, 11)];
+    for (int i = 0; i < rndmArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < rndmArray.GetLength(1); j++)
+        {
+            rndmArray[i, j] = new Random().Next(1, 100);
+        }
+    }
+    return rndmArray;
+}
+void ShowElement(int[,] array, int pos1, int pos2)
+{
+    int a = array.GetLength(0);
+    int b = array.GetLength(1);
+    if (pos1 <= a && pos2 <= b && pos1 > 0 && pos2 > 0)
+    {
+        Console.WriteLine($"Element in position {pos1} {pos2} = {array[pos1 - 1, pos2 - 1]}");
+    }
+    else
+    {
+        Console.WriteLine($"Not found element in position {pos1} {pos2}");
+    }
+}
+Console.Write("Input first position: ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second position: ");
+int y = Convert.ToInt32(Console.ReadLine());
+ShowElement(RandomArray(), x, y);
 
 // Задача 52. Задайте двумерный массив из целых чисел.
 // Найдите среднее арифметическое элементов в каждом столбце.
@@ -81,15 +81,21 @@ int[,] CreateArr()
 double[] MOC(int[,] ar)
 {
     double sum = 0;
-    int size = ar.GetLength(1);
-    double[] average = new double[size];
-    for (int i = 0; i < ar.GetLength(0); i++)
+    int size = ar.GetLength(0);
+    double[] average = new double[ar.GetLength(1)];
+    int x = 0;
+    for (int i = 0; i < average.Length; i++)
     {
-        for (int j = 0; j < ar.GetLength(1); j++)
+        for (int j = 0; j < ar.GetLength(0); j++)
         {
-            sum += ar[i, j];
-            average[i] = (sum / size) / ar.GetLength(1);
+            sum += ar[j, x];
+            for (int g = 0; g < ar.GetLength(1); g++)
+            {
+                average[i] = sum / size;
+            }
         }
+        x++;
+        sum = 0;
     }
     return average;
 }
