f7313c138ebf63393d6b699b757bb8790819a872
Efreitor2001 <pokimoha.123@gmail.com>
2022-07-18 20:09:44 +0300

    Uncomment  HW_3

--- Python_Seminars/HomeWork_3/HW_3.py
+++ Python_Seminars/HomeWork_3/HW_3.py
@@ -3,67 +3,67 @@ from operator import le
 from queue import Empty
 # 1. Задайте список из нескольких чисел.
 # Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётной позиции.
-# def Sum(list):
-#     sum = 0
-#     for i in range(len(list)):
-#         if i % 2 != 0:
-#             sum += list[i]
-#     return sum
+def Sum(list):
+    sum = 0
+    for i in range(len(list)):
+        if i % 2 != 0:
+            sum += list[i]
+    return sum
 
 
-# a = [1, 5, 7, 10, 11, 25, 231, 3, 6]
-# print(f'Sum of elements in odd positions = {Sum(a)}')
+a = [1, 5, 7, 10, 11, 25, 231, 3, 6]
+print(f'Sum of elements in odd positions = {Sum(a)}')
 
 # 2. Напишите программу, которая найдёт произведение пар чисел списка.
 # Парой считаем первый и последний элемент, второй и предпоследний и т.д.
-# def Product(list):
-#     j = len(list) - 1
-#     size = math.ceil(len(list) / 2)
-#     print(list)
-#     for i in range(size):
-#         print(f'{list[i]} * {list[j]} = {list[i] * list[j]}')
-#         j -= 1
+def Product(list):
+    j = len(list) - 1
+    size = math.ceil(len(list) / 2)
+    print(list)
+    for i in range(size):
+        print(f'{list[i]} * {list[j]} = {list[i] * list[j]}')
+        j -= 1
 
 
-# b = [2, 3, 4, 5, 6]
-# Product(b)
+b = [2, 3, 4, 5, 6]
+Product(b)
 
 # 3. Задайте список из вещественных чисел. Напишите программу, которая найдёт разницу между
 # максимальным и минимальным значением дробной части элементов.
-# def frac(list):
-#     for i in range(len(list)):
-#         list[i] = int(round(list[i] - int(list[i]), 2) * 100)
-#     return list
+def frac(list):
+    for i in range(len(list)):
+        list[i] = int(round(list[i] - int(list[i]), 2) * 100)
+    return list
 
 
-# def diff(list):
-#     max = list[0]
-#     min = list[0]
-#     for i in range(len(list)):
-#         if list[i] != 0:
-#             if list[i] > max:
-#                 max = list[i]
-#             elif list[i] < min:
-#                 min = list[i]
-#     return (max-min)/100
+def diff(list):
+    max = list[0]
+    min = list[0]
+    for i in range(len(list)):
+        if list[i] != 0:
+            if list[i] > max:
+                max = list[i]
+            elif list[i] < min:
+                min = list[i]
+    return (max-min)/100
 
 
-# c = [1.1, 1.61, 3.1, 5, 10.01]
-# print(c)
-# print(diff(frac(c)))
+c = [1.1, 1.61, 3.1, 5, 10.01]
+print(c)
+print(diff(frac(c)))
 
 
 # 4. Напишите программу, которая будет преобразовывать десятичное число в двоичное.
-# def bin(n):
-#     res = str("")
-#     while n != 0:
-#         res += str(n % 2)
-#         n //= 2
-#     return ''.join(reversed(res))
+def bin(n):
+    res = str("")
+    while n != 0:
+        res += str(n % 2)
+        n //= 2
+    return ''.join(reversed(res))
 
 
-# n = int(input('Enter Your number: '))
-# print(bin(n))
+n = int(input('Enter Your number: '))
+print(bin(n))
 
 # *5. Задайте число. Составьте список чисел Фибоначчи, в том числе для отрицательных индексов.
 def Fibonacci(n):
