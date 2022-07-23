8e07e551e77f19b09487c7b8dfa080ee60592d14
Efreitor2001 <pokimoha.123@gmail.com>
2022-07-23 07:34:57 +0300

    Done 4 task

--- Python_Seminars/HomeWork_4/HW_4.py
+++ Python_Seminars/HomeWork_4/HW_4.py
@@ -48,22 +48,48 @@
 
 # 3. Задайте последовательность чисел. Напишите программу,
 # которая выведет список неповторяющихся элементов исходной последовательности.
-def Unique(n):
-    x = 0
-    a = list(n.split())
-    b = []
-    i = 0
-    while i < len(a):
-        x = a[i]
-        c = 0
-        for j in range(len(a)):
-            if int(a[j]) == int(x):
-                c += 1
-        if c < 2:
-            b.append(int(x))
-        i += 1
-    return b
+# def Unique(n):
+#     x = 0
+#     a = list(n.split())
+#     b = []
+#     i = 0
+#     while i < len(a):
+#         x = a[i]
+#         c = 0
+#         for j in range(len(a)):
+#             if int(a[j]) == int(x):
+#                 c += 1
+#         if c < 2:
+#             b.append(int(x))
+#         i += 1
+#     return b
 
 
-num = input('Enter Your numbers separated by a space: ')
-print(f'Unique elements are: {Unique(num)}')
+# num = input('Enter Your numbers separated by a space: ')
+# print(f'Unique elements are: {Unique(num)}')
+
+# 4. Задана натуральная степень k.Сформировать случайным образом
+# список коэффициентов (значения от 0 до 100) многочлена и вывести на экран.
+import random
+
+
+def Polynomial(k):
+    s = ''
+    r = 0
+    for i in range(k, 0, -1):
+        r = random.randint(0,100)
+        if r == 0:
+            s += ''
+        elif r == 1:
+            s += str(f'x^{i} + ')
+        elif i != 1:
+            s += str(f'{r}x^{i} + ')
+        else:
+            s += str(f'{r}x ')
+    r = random.randint(0,100)
+    if r != 0:
+        s += str(f'+ {r} = 0')
+    else:
+        s += str(f'= 0')
+    return s
+print(Polynomial(5))
