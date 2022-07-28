e39cac278dcb407ba1edc7411bcc7ed45e181ce5
Efreitor2001 <pokimoha.123@gmail.com>
2022-07-28 18:52:27 +0300

    Done 3 task

--- Python_Seminars/HomeWork_5/HW_5.py
+++ Python_Seminars/HomeWork_5/HW_5.py
@@ -1,5 +1,5 @@
+from curses.ascii import isdigit
 import random
-from trace import Trace
 # 1. Создайте программу для игры с конфетами человек против человека.
 # Условие задачи: На столе лежит 2021 конфета. Играют два игрока делая ход друг после друга.
 # Первый ход определяется жеребьёвкой. За один ход можно забрать не более чем 28 конфет.
@@ -233,56 +233,84 @@ from trace import Trace
 # # 2. Создайте программу для игры в ""Крестики-нолики"".
 
 
-def PrintList(b):
-    for i in range(0, len(b)):
-        for j in range(0, len(b[i])):
-            print(b[i][j], end=' ')
-        print()
+# def PrintList(b):
+#     for i in range(0, len(b)):
+#         for j in range(0, len(b[i])):
+#             print(b[i][j], end=' ')
+#         print()
 
 
-def CheckWins(b):
-    if (b[0][0] == 'X' and b[0][1] == 'X' and b[0][2] == 'X' or b[0][0] == '0' and b[0][1] == '0' and b[0][2] == '0' or 
-    b[1][0] == 'X' and b[1][1] == 'X' and b[1][2] == 'X' or b[1][0] == '0' and b[1][1] == '0' and b[1][2] == '0' or 
-    b[2][0] == 'X' and b[2][1] == 'X' and b[2][2] == 'X' or b[2][0] == '0' and b[2][1] == '0' and b[2][2] == '0' or 
-    b[0][0] == 'X' and b[1][0] == 'X' and b[2][0] == 'X' or b[0][0] == '0' and b[1][0] == '0' and b[2][0] == '0' or 
-    b[0][1] == 'X' and b[1][1] == 'X' and b[2][1] == 'X' or b[0][1] == '0' and b[1][1] == '0' and b[2][1] == '0' or 
-    b[0][2] == 'X' and b[1][2] == 'X' and b[2][2] == 'X' or b[0][2] == '0' and b[1][2] == '0' and b[2][2] == '0' or 
-    b[0][0] == 'X' and b[1][1] == 'X' and b[2][2] == 'X' or b[0][0] == '0' and b[1][1] == '0' and b[2][2] == '0' or 
-    b[0][2] == 'X' and b[1][1] == 'X' and b[2][0] == 'X' or b[0][2] == '0' and b[1][1] == '0' and b[2][0] == '0'):
-        return True
-    else:
-        return False
+# def CheckWins(b):
+#     if (b[0][0] == 'X' and b[0][1] == 'X' and b[0][2] == 'X' or b[0][0] == '0' and b[0][1] == '0' and b[0][2] == '0' or
+#     b[1][0] == 'X' and b[1][1] == 'X' and b[1][2] == 'X' or b[1][0] == '0' and b[1][1] == '0' and b[1][2] == '0' or
+#     b[2][0] == 'X' and b[2][1] == 'X' and b[2][2] == 'X' or b[2][0] == '0' and b[2][1] == '0' and b[2][2] == '0' or
+#     b[0][0] == 'X' and b[1][0] == 'X' and b[2][0] == 'X' or b[0][0] == '0' and b[1][0] == '0' and b[2][0] == '0' or
+#     b[0][1] == 'X' and b[1][1] == 'X' and b[2][1] == 'X' or b[0][1] == '0' and b[1][1] == '0' and b[2][1] == '0' or
+#     b[0][2] == 'X' and b[1][2] == 'X' and b[2][2] == 'X' or b[0][2] == '0' and b[1][2] == '0' and b[2][2] == '0' or
+#     b[0][0] == 'X' and b[1][1] == 'X' and b[2][2] == 'X' or b[0][0] == '0' and b[1][1] == '0' and b[2][2] == '0' or
+#     b[0][2] == 'X' and b[1][1] == 'X' and b[2][0] == 'X' or b[0][2] == '0' and b[1][1] == '0' and b[2][0] == '0'):
+#         return True
+#     else:
+#         return False
 
 
-a = 3
-lst = ['*'] * 3
-p = 0
-i = 0
-j = 0
-step = ''
-for i in range(a):
-    lst[i] = ['*'] * a
-PrintList(lst)
-for g in range(9):
-    if g % 2 == 0:
-        step = "X"
-    else:
-        step = "0"
-    i = int(input()) - 1
-    j = int(input()) - 1
-    while int(i) < 0 or int(i) > 2 or int(j) < 0 or int(j) > 2 or lst[i][j] != '*':
-        print('Error! Specify other coordinates')
-        i = int(input()) - 1
-        j = int(input()) - 1
-    lst[i][j] = step
-    PrintList(lst)
-    CheckWins(lst)
-    if CheckWins(lst) == True:
-        p = step
-        break
-if p == 'X':
-    print("Wins X")
-elif p == '0':
-    print("Wins 0")
-else:
-    print("Friendship won")
+# a = 3
+# lst = ['*'] * 3
+# p = 0
+# i = 0
+# j = 0
+# step = ''
+# for i in range(a):
+#     lst[i] = ['*'] * a
+# PrintList(lst)
+# for g in range(9):
+#     if g % 2 == 0:
+#         step = "X"
+#     else:
+#         step = "0"
+#     i = int(input()) - 1
+#     j = int(input()) - 1
+#     while int(i) < 0 or int(i) > 2 or int(j) < 0 or int(j) > 2 or lst[i][j] != '*':
+#         print('Error! Specify other coordinates')
+#         i = int(input()) - 1
+#         j = int(input()) - 1
+#     lst[i][j] = step
+#     PrintList(lst)
+#     CheckWins(lst)
+#     if CheckWins(lst) == True:
+#         p = step
+#         break
+# if p == 'X':
+#     print("Wins X")
+# elif p == '0':
+#     print("Wins 0")
+# else:
+#     print("Friendship won")
+
+# 3. Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
+import itertools
+
+
+def compress(text):
+    for char, same in itertools.groupby(text):
+        count = sum(1 for _ in same)
+        yield str(count)+char
+
+
+def uncompress(rle):
+    decode = '' 
+    count = '' 
+    for i in rle: 
+        if i.isdigit():
+            count += i 
+        else:  
+            decode += i * int(count) 
+            count = '' 
+    return decode 
+
+
+s = input('Enter: ')
+s1 = ''.join(compress(s))
+print(s1)
+s2 = uncompress(s1)
+print(s2)
