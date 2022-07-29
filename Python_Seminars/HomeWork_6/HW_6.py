6186d757c5f1c0d8b656dc1b0ef69aafca04d5d8
Efreitor2001 <pokimoha.123@gmail.com>
2022-07-29 05:48:28 +0300

    Done 3 task

--- Python_Seminars/HomeWork_6/HW_6.py
+++ Python_Seminars/HomeWork_6/HW_6.py
@@ -9,25 +9,53 @@
 
 # Самая далёкая планета
 
-def find_farthest_orbit(tpl):
-    p = 3.14
-    lst = []
-    result = ()
-    for i in tpl:
-        a, b = i
-        if a != b:
-            lst.append(a)
-            lst.append(b)
-    s = [p * lst[i] * lst[i + 1] for i in range(0, len(lst), 2)]  # использовал списочное выражение =)
-    max_s = s[0]
-    for i in s:
-        if i > max_s:
-            max_s = i
-    for i in range(0, len(lst), 2):
-        if p * lst[i] * lst[i + 1] == max_s:
-            result = lst[i], lst[i + 1]
-    return result
+# def find_farthest_orbit(tpl):
+#     p = 3.14
+#     lst = []
+#     result = ()
+#     for i in tpl:
+#         a, b = i
+#         if a != b:
+#             lst.append(a)
+#             lst.append(b)
+#     s = [p * lst[i] * lst[i + 1] for i in range(0, len(lst), 2)]  # использовал списочное выражение =)
+#     max_s = s[0]
+#     for i in s:
+#         if i > max_s:
+#             max_s = i
+#     for i in range(0, len(lst), 2):
+#         if p * lst[i] * lst[i + 1] == max_s:
+#             result = lst[i], lst[i + 1]
+#     return result
+#
+#
+# orbits = [(1, 3), (2.5, 10), (7, 2), (6, 6), (4, 3)]
+# print(*find_farthest_orbit(orbits))
+
+# Пам-парам парам-пам парам
+def param(s):
+    a = s.split()
+    count = 0
+    cl = []
+    check = 'ауоыэяюёиеaeiouy'
+    for i in a:
+        for j in i:
+            for g in check:
+                if g in j:
+                    count += 1
+        if count > 0:
+            cl.append(count)
+        count = 0
+    y = cl[0]
+    b = list(filter(lambda x: x == y, cl))  # использовал lamda-функцию в сочетании с функцией высшего порядка =)
+    if len(cl) == len(b):
+        return True
+    else:
+        return False
 
 
-orbits = [(1, 3), (2.5, 10), (7, 2), (6, 6), (4, 3)]
-print(*find_farthest_orbit(orbits))
+verse = str(input('Enter Your verse: '))
+if param(verse):
+    print("Парам пам-пам")
+else:
+    print("Пам парам")
