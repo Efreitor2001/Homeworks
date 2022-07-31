26060acdb1e25b87f77b63256fdfe904fbe6e013
Efreitor2001 <pokimoha.123@gmail.com>
2022-07-31 16:19:59 +0300

    Done all HW_6

--- Python_Seminars/HomeWork_6/HW_6.py
+++ Python_Seminars/HomeWork_6/HW_6.py
@@ -1,90 +1,111 @@
 # Мимикрия
-# transformation = lambda x: x
-# values = [1, 23, 42, "asdfg"]
-# transformed_values = list(map(transformation, values))
-# if values == transformed_values:
-#     print('ok')
-# else:
-#     print('fail')
+transformation = lambda x: x
+values = [1, 23, 42, "asdfg"]
+transformed_values = list(map(transformation, values))
+if values == transformed_values:
+    print('ok')
+else:
+    print('fail')
+
 
 # Самая далёкая планета
 
-# def find_farthest_orbit(tpl):
-#     p = 3.14
-#     lst = []
-#     result = ()
-#     for i in tpl:
-#         a, b = i
-#         if a != b:
-#             lst.append(a)
-#             lst.append(b)
-#     s = [p * lst[i] * lst[i + 1] for i in range(0, len(lst), 2)]  # использовал списочное выражение =)
-#     max_s = s[0]
-#     for i in s:
-#         if i > max_s:
-#             max_s = i
-#     for i in range(0, len(lst), 2):
-#         if p * lst[i] * lst[i + 1] == max_s:
-#             result = lst[i], lst[i + 1]
-#     return result
-#
-#
-# orbits = [(1, 3), (2.5, 10), (7, 2), (6, 6), (4, 3)]
-# print(*find_farthest_orbit(orbits))
+def find_farthest_orbit(tpl):
+    p = 3.14
+    lst = []
+    result = ()
+    for i in tpl:
+        a, b = i
+        if a != b:
+            lst.append(a)
+            lst.append(b)
+    s = [p * lst[i] * lst[i + 1] for i in range(0, len(lst), 2)]  # использовал списочное выражение =)
+    max_s = s[0]
+    for i in s:
+        if i > max_s:
+            max_s = i
+    for i in range(0, len(lst), 2):
+        if p * lst[i] * lst[i + 1] == max_s:
+            result = lst[i], lst[i + 1]
+    return result
+
+
+orbits = [(1, 3), (2.5, 10), (7, 2), (6, 6), (4, 3)]
+print(*find_farthest_orbit(orbits))
+
 
 # Пам-парам парам-пам парам
-# def param(s):
-#     a = s.split()
-#     count = 0
-#     cl = []
-#     check = 'ауоыэяюёиеaeiouy'
-#     for i in a:
-#         for j in i:
-#             for g in check:
-#                 if g in j:
-#                     count += 1
-#         if count > 0:
-#             cl.append(count)
-#         count = 0
-#     y = cl[0]
-#     b = list(filter(lambda x: x == y, cl))  # использовал lamda-функцию в сочетании с функцией высшего порядка =)
-#     if len(cl) == len(b):
-#         return True
-#     else:
-#         return False
-#
-#
-# verse = str(input('Enter Your verse: '))
-# if param(verse):
-#     print("Парам пам-пам")
-# else:
-#     print("Пам парам")
+def param(s):
+    a = s.split()
+    count = 0
+    cl = []
+    check = 'ауоыэяюёиеaeiouy'
+    for i in a:
+        for j in i:
+            for g in check:
+                if g in j:
+                    count += 1
+        if count > 0:
+            cl.append(count)
+        count = 0
+    y = cl[0]
+    b = list(filter(lambda x: x == y, cl))  # использовал lamda-функцию в сочетании с функцией высшего порядка =)
+    if len(cl) == len(b):
+        return True
+    else:
+        return False
+
+
+verse = str(input('Enter Your verse: '))
+if param(verse):
+    print("Парам пам-пам")
+else:
+    print("Пам парам")
+
 
 # Все равны, как на подбор
-# def same_by(characteristic, objects):
-#     a = []
-#     for i in objects:
-#         if characteristic(i) == 0:
-#             a.append(True)
-#         else:
-#             a.append(False)
-#     y = a[0]
-#     b = list(filter(lambda x: x == y, a))
-#     if len(a) == len(b):
-#         return True
-#     else:
-#         return False
-#
-#
-# values = [0, 2, 4, 6]
-# if same_by(lambda x: x % 2, values):
-#     print('same')
-# else:
-#     print('different')
+def same_by(characteristic, objects):
+    a = []
+    for i in objects:
+        if characteristic(i) == 0:
+            a.append(True)
+        else:
+            a.append(False)
+    y = a[0]
+    b = list(filter(lambda x: x == y, a))
+    if len(a) == len(b):
+        return True
+    else:
+        return False
+
+
+values = [0, 2, 4, 6]
+if same_by(lambda x: x % 2, values):
+    print('same')
+else:
+    print('different')
+
 
 # Безимянная задачка
-# def print_operation_table(operation, num_rows, num_columns):
-#     for i in range(1, num_rows + 1):
-#         for j in range(i, i * num_columns + 1, i):
-#             print(j, end='\t')
-#         print()
+def print_operation_table(operation, num_rows, num_columns):
+    if operation == '*':
+        for i in range(1, num_rows + 1):
+            for j in range(i, i * num_columns + 1, i):
+                print(j, end='\t')
+            print()
+    if operation == '+':
+        for i in range(1, num_rows + 1):
+            for j in range(i, i + num_columns + 1, i):
+                print(j, end='\t')
+            print()
+    if operation == '**':
+        for i in range(1, num_rows + 1):
+            for j in range(i, i ** num_columns + 1, i):
+                print(j, end='\t')
+            print()
+
+
+nr = int(input('Enter count rows: '))
+op = str(input('Enter operation: '))
+nc = int(input('Enter count columns: '))
+print_operation_table(op, nr, nc)
---
+++ Python_Seminars/HoneWork_7/input.py
@@ -0,0 +1,2 @@
+n = str(input('Enter name: '))
+p = str(input('Enter phone: '))
