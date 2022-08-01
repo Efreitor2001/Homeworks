78d6189f46b0bf2d71df39ede00a50a75757813c
Efreitor2001 <pokimoha.123@gmail.com>
2022-08-02 00:09:23 +0300

    proryv2

--- Python_Seminars/HomeWork_7/PB_writer.py
+++ Python_Seminars/HomeWork_7/PB_writer.py
@@ -1,12 +1,14 @@
 from input import ln
 from input import fn
 from input import p
+from input import desc
 
-lst = [f'Last name: {ln}', f'First name: {fn}', f'Phone: {p}']
+lst = [f'Last name: {ln}', f'First name: {fn}', f'Phone: {p}', f'Description: {desc}']
 
 
 def write_pb():
     f = open('/home/efreitor2001/Homeworks/Python_Seminars/HomeWork_7/phonebook.txt', 'a')
     for i in lst:
         f.write('\n' + i)
+    f.write('\n')
     f.close()
--- Python_Seminars/HomeWork_7/input.py
+++ Python_Seminars/HomeWork_7/input.py
@@ -1,3 +1,4 @@
 ln = str(input('Enter last name: '))
 fn = str(input('Enter name: '))
 p = str(input('Enter phone: '))
+desc = str(input('Enter a description: '))
