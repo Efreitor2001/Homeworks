d24c4d3e50bac20e027290062dcf90d317814c05
Efreitor2001 <pokimoha.123gmail.com>
2022-09-02 21:47:18 +0300

    Fixed HW_1

--- Java_Seminars/HomeWork_1/src/HW_1.java
+++ Java_Seminars/HomeWork_1/src/HW_1.java
@@ -2,42 +2,51 @@ import java.util.Scanner;
 
 public class HW_1 {
     public static void main(String[] args) {
-        int check;
-        do {
-            Scanner s = new Scanner(System.in);
-            System.out.print("Введите первое число: ");
-            float n1 = s.nextFloat();
-            System.out.print("Введите действие (+, -, * или /): ");
-            String act = s.next();
-            System.out.print("Введите второе число: ");
-            float n2 = s.nextFloat();
-            float res = 0;
-            switch (act) {
-                case ("+"):
-                    res = n1 + n2;
-                    break;
-                case ("-"):
-                    res = n1 - n2;
-                    break;
-                case ("*"):
-                    res = n1 * n2;
-                    break;
-                case ("/"):
-                    res = n1 / n2;
-                    break;
-                default:
-                    System.out.println("Неправильное действие!");
-                    break;
-            }
-            System.out.println(res);
-            System.out.print("Хотите посчитать что-то ещё? (Да/Нет): ");
-            String ch = s.next().toLowerCase();
-            if (ch.equals("да")) {
-                check = 1;
-            } else {
-                check = 0;
-            }
-        } while (check == 1);
+        Scanner s = new Scanner(System.in);
+        try (s) {
+            do {
+                System.out.print("Введите первое число: ");
+                float num1 = s.nextFloat();
+                System.out.print("Введите действие (+, -, * или /): ");
+                String action = s.next();
+                System.out.print("Введите второе число: ");
+                float num2 = s.nextFloat();
+                System.out.println(calc(num1, num2, action));
+            } while (check(s));
+        }
         System.out.print("Пока-пока");
+    }
+
+    static float calc(float n1, float n2, String act) {
+        float res = 0;
+        switch (act) {
+            case ("+"):
+                res = n1 + n2;
+                return res;
+            case ("-"):
+                res = n1 - n2;
+                return res;
+            case ("*"):
+                res = n1 * n2;
+                return res;
+            case ("/"):
+                res = n1 / n2;
+                return res;
+            default:
+                System.out.println("Неправильное действие!");
+                return 0;
+        }
+    }
+
+    static boolean check(Scanner s) {
+        boolean chk;
+        System.out.print("Хотите посчитать что-то ещё? (Да/Нет): ");
+        String ch = s.next().toLowerCase();
+        if (ch.equals("да")) {
+            chk = true;
+        } else {
+            chk = false;
+        }
+        return chk;
     }
 }
