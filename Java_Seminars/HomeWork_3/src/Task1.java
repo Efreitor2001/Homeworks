1ca85d281a4c98bbf43d9d514ac7e1526976cb63
Efreitor2001 <pokimoha.123gmail.com>
2022-09-09 02:48:14 +0300

    Done task 1 in HW_2

---
+++ Java_Seminars/HomeWork_3/src/Task1.java
@@ -0,0 +1,54 @@
+//Реализовать алгоритм сортировки слиянием
+public class Task1 {
+    public static void main(String[] args) {
+        int[] l = {5, 2, 3, 1, 4};
+        int[] res = sortArray(l);
+        for (int i : res) {
+            System.out.print(i + " ");
+        }
+    }
+
+    public static int[] sortArray(int[] array) {
+        if (array == null) {
+            return null;
+        }
+        if (array.length < 2) {
+            return array;
+        }
+
+        int[] arrayB = new int[array.length / 2];
+        System.arraycopy(array, 0, arrayB, 0, array.length / 2);
+
+        int[] arrayC = new int[array.length - arrayB.length];
+        System.arraycopy(array, arrayB.length, arrayC, 0, array.length - arrayB.length);
+
+        sortArray(arrayB);
+        sortArray(arrayC);
+
+        mergeArray(array, arrayB, arrayC);
+
+        return array;
+    }
+
+    private static void mergeArray(int[] array, int[] arrayB, int[] arrayC) {
+
+        int positionB = 0;
+        int positionC = 0;
+
+        for (int c = 0; c < array.length; c++) {
+            if (positionB == arrayB.length) {
+                array[c] = arrayC[positionC];
+                positionC++;
+            } else if (positionC == arrayC.length) {
+                array[c] = arrayB[positionB];
+                positionB++;
+            } else if (arrayB[positionB] < arrayC[positionC]) {
+                array[c] = arrayB[positionB];
+                positionB++;
+            } else {
+                array[c] = arrayC[positionC];
+                positionC++;
+            }
+        }
+    }
+}
