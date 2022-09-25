a57169155cd8b07d2935d07fd6e1bc72d9089cc2
Efreitor2001 <pokimoha.123gmail.com>
2022-09-25 04:58:29 +0300

    Edite task 1 in HW_5

--- Java_Seminars/HomeWork_5/src/Task1.java
+++ Java_Seminars/HomeWork_5/src/Task1.java
@@ -1,13 +1,24 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.TreeMap;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 //Дана строка. Необходимо написать метод, который отсортирует слова в строке по длине с помощью TreeMap.
 //Строки с одинаковой длиной не должны “потеряться”.
 public class Task1 {
     public static void main(String[] args) {
+        Logger lg = Logger.getLogger(Task1.class.getName());
         String s = "Мороз и солнце день чудесный Еще ты дремлешь друг прелестный Пора красавица проснись";
-        ArrayList<String> s1 = new ArrayList<>(List.of(s.split(" ")));
+        try {
+            lg.log(Level.INFO, sort(s).values().toString());
+        } catch (Exception ex) {
+            lg.log(Level.WARNING, ex.getMessage());
+        }
+    }
+
+    public static TreeMap<Integer, String> sort(String str) {
+        ArrayList<String> s1 = new ArrayList<>(List.of(str.split(" ")));
         ArrayList<String> s3 = new ArrayList<>();
         TreeMap<Integer, String> tm = new TreeMap<>();
         while (s1.size() != 0) {
@@ -21,6 +32,6 @@ public class Task1 {
             tm.put(s3.get(0).length(), s3.toString());
             s3.clear();
         }
-        System.out.println(tm);
+        return tm;
     }
 }
